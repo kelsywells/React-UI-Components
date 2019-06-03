0bc95ebf6783dce63bc1c48886993943f71ab809
Kelsy <kelsywells@gmail.com>
2019-06-02 18:26:20 -0700

    completed NumberButton and ActionButton but getting some confusing errors. Need more stuff but a bit stuck at the moment.

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Button.css';
+
+
+//- Create an `<ActionButton />` component that will be used for the `zero` character and the `clear` button.
+
+const ActionButton = [
+    {name: 'zero', buttonStyle: 'actBtn', text: '0'},
+    {name: 'clear', buttonStyle: 'actBtn', text: 'clear'}
+]
+
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Display.css';
+
+// - Create a `<CalculatorDisplay />` component that will be used as the `calculator display`
+
+const CalculatorDisplay = () => {
+    
+}
+
+export default CalculatorDisplay;
